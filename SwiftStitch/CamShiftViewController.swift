7180681b4f0b0283ade2683db9dc539e319cbc28
Conti <agc11d@gmail.com>
2017-01-24 17:14:13 -0500

    feat: playing video

---
+++ SwiftStitch/CamShiftViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CamShiftViewController.swift
+//  SwiftStitch
+//
+//  Created by Andrew Conti on 1/24/17.
+//  Copyright © 2017 ellipsis.com. All rights reserved.
+//
+
+import UIKit
+
+class CamShiftViewController: NSObject {
+
+}
